540b4443ea637a8b0339d37740697b5ba5838f09
ashishkumar <meashish40@gmail.com>
2019-05-31 11:29:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view){
+        EditText editText=(EditText) findViewById(R.id.editText);
+        int val = Integer.parseInt(editText.getText().toString() );
+        Toast.makeText(this, "Amount in Rs "+(69*val), Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
